aa69714373c5c71f4491e60908501e4072d85b53
Christopher Beltran <christod.beltran@gmail.com>
2018-06-11 05:16:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 def fizzbuzz(number)
   by3 = (number % 3 == 0)
+  by5 = (number % 5 == 0)
+  
+  case
+    when by3 && by5
+      puts "FizzBuzz"
+    when by3
+      puts "Fizz"
+    when by5
+      puts "Buzz"
+    end
+  end
