2729939c1f9fbb2c49d7cbc52c57a867ca1fccdc
Christopher Beltran <christod.beltran@gmail.com>
2018-06-11 05:22:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@ def fizzbuzz(number)
   by3 = (number % 3 == 0)
   by5 = (number % 5 == 0)
   
-  if by3 && by5
+  when by3 && by5
       "FizzBuzz"
-    elseif by3
+  when by3
       "Fizz"
-    elseif by5
+  when by5
       "Buzz"
     else
       nil
